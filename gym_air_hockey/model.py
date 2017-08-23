2f28a98251af3707c89b163a331012f25a74c620
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2017-08-23 00:48:12 -0400

    Changed NN model to ConvLSTM from simple CNN

--- gym_air_hockey/model.py
+++ gym_air_hockey/model.py
@@ -1,32 +1,30 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras import regularizers
+from keras.layers.wrappers import TimeDistributed
 
 def build_model():
     
-    l1 = 0.0000
-    l2 = 0.01
+    l1 = 0.0001
+    l2 = 0.00001
   
     model = Sequential([
-                Conv2D(16, 16, activation='relu', name='conv1', input_shape=(128, 128, 3), 
-                       kernel_regularizer=regularizers.l2(l2), activity_regularizer=regularizers.l1(l1)),
+                ConvLSTM2D(8, 16, activation='relu', name='conv1', input_shape=(None, 128, 128, 3),
+                           kernel_regularizer=regularizers.l1(l1)),
                 Dropout(0.2, name='dropout1'),
                 MaxPooling2D(3, name='pool1'),
-                Conv2D(32, 16, activation='relu', name='conv2', 
-                       kernel_regularizer=regularizers.l2(l2), activity_regularizer=regularizers.l1(l1)),
-#                Dropout(0.1, name='dropout2'),
+                Conv2D(16, 16, activation='relu', name='conv2', kernel_regularizer=regularizers.l1(l1)),
+                Dropout(0.2, name='dropout2'),
                 MaxPooling2D(3, name='pool2'),
                 Flatten(name='flatten'),
-                Dense(256, activation='relu',    name='dense1', 
-                       kernel_regularizer=regularizers.l2(l2), activity_regularizer=regularizers.l1(l1)),
+                Dense(256, activation='relu',    name='dense1', kernel_regularizer=regularizers.l1(l1)),
+                Dropout(0.2, name='dropout3'),
+                Dense(128, activation='relu',    name='dense2', kernel_regularizer=regularizers.l1(l1)),
                 Dropout(0.2, name='dropout4'),
-                Dense(128, activation='relu',    name='dense2', 
-                       kernel_regularizer=regularizers.l2(l2), activity_regularizer=regularizers.l1(l1)),
-#                Dropout(0.1, name='dropout5'),
-                Dense(64,  activation='relu',    name='dense3', 
-                       kernel_regularizer=regularizers.l2(l2), activity_regularizer=regularizers.l1(l1)),
-#                Dropout(0.1, name='dropout6'),
+                Dense(64,  activation='relu',    name='dense3', kernel_regularizer=regularizers.l1(l1)),
+                Dropout(0.2, name='dropout5'),
                 Dense(9,   activation='softmax', name='out')
                 ])
-    model.compile(loss='categorical_crossentropy',  optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy',  optimizer='rmsprop', metrics=['accuracy'])
     return model
