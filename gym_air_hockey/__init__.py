94158bacec3d0d473c4ed880835682a4a08ba97d
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2017-08-25 01:32:54 -0400

    Removed keras model from the module to decrease coupling

--- gym_air_hockey/__init__.py
+++ gym_air_hockey/__init__.py
@@ -1,9 +1,8 @@
-from gym_air_hockey.processor import DataProcessor
-from gym_air_hockey.model import conv_model, convlstm_model
-
 from gym.envs.registration import register
 
 register(
     id='AirHockey-v0',
     entry_point='gym_air_hockey.envs:AirHockeyEnv'
 )
+
+from gym_air_hockey.processor import DataProcessor
