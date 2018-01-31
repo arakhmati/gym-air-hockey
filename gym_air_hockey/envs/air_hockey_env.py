0967e4020d98faca874e38153be0b96e71bd6e0c
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-01-31 06:53:22 -0500

    Refactored code by adding DataProcessor inside of the environment. Merged grayscale with rgb

--- gym_air_hockey/envs/air_hockey_env.py
+++ gym_air_hockey/envs/air_hockey_env.py
@@ -6,65 +6,79 @@ from air_hockey import AirHockey
 from gym_air_hockey import DataProcessor
 
 class AirHockeyEnv(gym.Env):
-    def __init__(self, video_file='reinforcement.avi'):
+    def __init__(self):
 
-        self.game = AirHockey(video_file=video_file)
+        self.game = AirHockey()
         self.processor = DataProcessor()
 
-        self.action_space = gym.spaces.Discrete(len(self.processor.actions))
-        self.n_actions = self.action_space.n
-
         self.height = self.width = self.processor.dim
+
+        self.action_space = gym.spaces.Discrete(len(self.processor.actions))
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(9, self.height, self.width))
         self.reward_range = (-1.0, 1.0)
 
+        self.n_actions = self.action_space.n
+
         self.viewer = None
 
-    def step(self, action, adversarial_action=None):
-        game_info = self.game.step(action=action, adversarial_action=adversarial_action)
+    def update(self, video_file=None, mode=None):
+        if video_file is not None:
+            self.game = AirHockey(video_file=video_file)
+        if mode is not None:
+            self.processor = DataProcessor(mode=mode)
 
-        state = game_info.frame
+    def step(self, robot_action=None, human_action=None, debug=False):
+
+        robot_action = self.processor.process_action(robot_action)
+        human_action = self.processor.process_action(human_action)
+
+        game_info = self.game.step(robot_action=robot_action, human_action=human_action)
         terminal = game_info.scored is not None
         
-        action = self.processor.action_to_label(game_info.action)
-        adversarial_action = self.processor.action_to_label(game_info.adversarial_action)
+        robot_action = self.processor.action_to_label(game_info.robot_action)
+        human_action = self.processor.action_to_label(game_info.human_action)
 
         # Reward
         reward = 0.0
         if game_info.scored == 'top':
-            print('GAME INFO: Goal ☹')
+            if debug: print('GAME INFO: Goal ☹')
             reward = -1.0
         elif game_info.scored == 'bottom':
-            print('GAME INFO: Goal ☺')
+            if debug: print('GAME INFO: Goal ☺')
             reward = 1.0
         elif game_info.puck_was_hit:
-            print('GAME INFO: puck was hit ☺')
+            if debug: print('GAME INFO: puck was hit ☺')
             reward = 0.5
         elif game_info.hit_the_border:
-            print('GAME INFO: hit the border ☹')
+            if debug: print('GAME INFO: hit the border ☹')
             reward = -0.5
         elif game_info.puck_is_at_the_bottom:
             if game_info.distance_decreased:
-                print('GAME INFO: decreased distance ☺')
+                if debug: print('GAME INFO: decreased distance ☺')
                 reward = 0.5
             else:
-                print('GAME INFO: increased distance ☹')
+                if debug: print('GAME INFO: increased distance ☹')
                 reward = -0.5
         else:
-            if action == 4:
-                print('GAME INFO: stanidng still ☺')
+            if robot_action == 4:
+                if debug: print('GAME INFO: standing still ☺')
                 reward = 0.5
             else:
-                print('GAME INFO: not standing still ☹')
+                if debug: print('GAME INFO: not standing still ☹')
                 reward = -0.5
-            
+    
         reward = np.clip(reward, -1.0, 1.0)
+        state = self.processor.process_observation(game_info.frame)
 
-        return state, reward, terminal, {'action': action, 'adversarial_action': adversarial_action}
+        return state, reward, terminal, {'robot_action': robot_action, 'human_action': human_action}
 
-    def _reset(self):
-        game_info = self.game.reset()
-        return game_info.frame
+    def reset(self):
+        # Fill in the current_state
+        stand_action = self.processor.process_action(4)
+        for _ in range(3):
+            game_info = self.game.step(robot_action=stand_action, human_action=stand_action)
+            state = self.processor.process_observation(game_info.frame)
+        return state
 
 
 
--- gym_air_hockey/processor.py
+++ gym_air_hockey/processor.py
@@ -20,9 +20,7 @@ class DataProcessor(object):
         self.metrics_names = []
 
         self.mode = mode
-        self.frame_buffer = np.zeros((3 * 3, self.dim, self.dim), dtype=np.float32)
-
-
+        self.state_buffer = np.zeros((3 * 3, self.dim, self.dim), dtype=np.float32)
 
     def process_step(self, observation, reward, done, info):
         observation = self.process_observation(observation)
@@ -30,53 +28,45 @@ class DataProcessor(object):
         info = self.process_info(info)
         return observation, reward, done, info
 
-    def _resize_observation(self, observation):
-        observation = Image.fromarray(observation)
-        observation = np.array(observation.resize((self.dim, self.dim)))
-        observation = observation.transpose((2, 0 ,1))
-        return observation
-
-    @staticmethod
-    def _normalize_observation(observation):
-        observation = observation - 128.0 # normalize
-        observation = observation / 128.- # scale
-        return np.copy(observation)
-
-    def process_grayscale(self, observation):
-        gray = self._normalize_observation(gray)
-        return np.copy(gray)
-
     def process_observation(self, observation):
-        observation = self._resize_observation(observation)
+
+        def resize_observation(observation):
+            observation = Image.fromarray(observation)
+            observation = np.array(observation.resize((self.dim, self.dim)))
+            observation = observation.transpose((2, 0 ,1))
+            return observation
+
+        def normalize_observation(observation):
+            observation = observation - 128.0 # normalize
+            observation = observation / 128.0 # scale
+            return np.copy(observation)
+
+        observation = resize_observation(observation)
         observation = observation.astype(np.float32)
 
-        self.frame[2*3:(2+1)*3] = np.copy(self.frame[1*3:(1+1)*3])
-        self.frame[1*3:(1+1)*3] = np.copy(self.frame[0*3:(0+1)*3])
-        self.frame[0*3:(0+1)*3] = np.copy(observation)
+        self.state_buffer[2*3:(2+1)*3] = np.copy(self.state_buffer[1*3:(1+1)*3])
+        self.state_buffer[1*3:(1+1)*3] = np.copy(self.state_buffer[0*3:(0+1)*3])
+        self.state_buffer[0*3:(0+1)*3] = np.copy(observation)
 
         if self.mode == 'rgb':
-            return self._normalize_observation(observation)
-        else if self.mode == 'grayscale_diff':
-            transposed = np.uint8(self.frames.transpose((1, 2, 0)))
+            return normalize_observation( self.state_buffer)
+        elif self.mode == 'gray-diff':
+            transposed = np.uint8(self.state_buffer.transpose((1, 2, 0)))
             # Take the double difference
             diff = transposed[:,:,6:9] - transposed[:, :, 3:6] - transposed[:, :, 0:3]
-
             # https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python
             def rgb2gray(rgb):
                 r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
                 gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
                 return gray
-            gray = rgb2gray(diff)
-            gray = gray.reshape(1, 128, 128)
+            gray = rgb2gray(diff)\
+                   .reshape(1, 128, 128)
+            return normalize_observation(gray)
         else:
             raise ValueError('Processing mode is not supported')
 
-
-        return self._normalize_observation(observation)
-
-
     def process_reward(self, reward):
-        return reward
+        return np.clip(reward, -1.0, 1.0)
 
     def process_info(self, info):
         return info
