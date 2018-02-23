36684530a84af97ba21b6d13b16d9d32e3097d16
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-02-23 15:38:29 -0500

    Added real-life episode scenarios. Added a reward for the robot to stay in the target zone when the puck is on the opposite side

--- gym_air_hockey/envs/air_hockey_env.py
+++ gym_air_hockey/envs/air_hockey_env.py
@@ -20,6 +20,60 @@ class AirHockeyEnv(gym.Env):
         self.n_actions = self.action_space.n
 
         self.viewer = None
+        
+        self.reset()
+        
+    def reset(self):
+        
+        episodes = ['normal'] * 16 + ['missing'] * 3 + ['random']
+        self.episode = episodes[np.random.randint(len(episodes))]
+        print('Episode type: {}'.format(self.episode))
+        
+        self.use_object = {'arm': True,
+                           'puck': True,
+                           'top_mallet': True}
+        
+        if self.episode == 'normal':
+            pass
+        elif self.episode == 'missing':
+            random_num = np.random.randint(6)
+            if random_num == 0:
+                self.use_object['arm'] = False
+                self.use_object['puck'] = False
+                self.use_object['top_mallet'] = False
+            elif random_num == 1:
+                self.use_object['arm'] = False
+                self.use_object['puck'] = False
+                self.use_object['top_mallet'] = True
+            elif random_num == 2:
+                self.use_object['arm'] = False
+                self.use_object['puck'] = True
+                self.use_object['top_mallet'] = False
+            elif random_num == 3:
+                self.use_object['arm'] = False
+                self.use_object['puck'] = True
+                self.use_object['top_mallet'] = True
+            elif random_num == 4:
+                self.use_object['arm'] = True
+                self.use_object['puck'] = False
+                self.use_object['top_mallet'] = False
+            elif random_num == 5:
+                self.use_object['arm'] = True
+                self.use_object['puck'] = False
+                self.use_object['top_mallet'] = True
+        
+        if self.episode != 'random':
+            # Reset the environment
+            self.game.reset(use_object=self.use_object)
+            # Fill in the current_state
+            stand_action = self.processor.process_action(4)
+            for _ in range(3):
+                game_info = self.game.step(robot_action=stand_action, human_action=stand_action)
+                state = self.processor.process_observation(game_info.frame)    
+        else:
+            state = np.random.randint(0, 255, (9, 128, 128), dtype=np.uint8)
+            
+        return state
 
     def update(self, video_file=None, mode=None):
         if video_file is not None:
@@ -32,7 +86,7 @@ class AirHockeyEnv(gym.Env):
         robot_action = self.processor.process_action(robot_action)
         human_action = self.processor.process_action(human_action)
 
-        game_info = self.game.step(robot_action=robot_action, human_action=human_action)
+        game_info = self.game.step(robot_action=robot_action, human_action=human_action, use_object=self.use_object)
         terminal = game_info.scored is not None
         
         robot_action = self.processor.action_to_label(game_info.robot_action)
@@ -40,47 +94,66 @@ class AirHockeyEnv(gym.Env):
 
         # Reward
         reward = 0.0
-        if game_info.scored == 'top':
-            if debug: print('GAME INFO: Goal ☹')
-            reward = -1.0
-        elif game_info.scored == 'bottom':
-            if debug: print('GAME INFO: Goal ☺')
-            reward = 1.0
-        elif game_info.puck_was_hit:
-            if debug: print('GAME INFO: puck was hit ☺')
-            reward = 0.5
-        elif game_info.hit_the_border:
-            if debug: print('GAME INFO: hit the border ☹')
-            reward = -0.5
-        elif game_info.puck_is_at_the_bottom:
-            if game_info.distance_decreased:
-                if debug: print('GAME INFO: decreased distance ☺')
+        if self.episode == 'normal':
+            if game_info.scored == 'top':
+                if debug: print('GAME INFO: Goal ☹')
+                reward = -1.0
+            elif game_info.scored == 'bottom':
+                if debug: print('GAME INFO: Goal ☺')
+                reward = 1.0
+            elif game_info.puck_was_hit:
+                if debug: print('GAME INFO: puck was hit ☺')
                 reward = 0.5
-            else:
-                if debug: print('GAME INFO: increased distance ☹')
+            elif game_info.hit_the_border:
+                if debug: print('GAME INFO: hit the border ☹')
                 reward = -0.5
-        else:
-            if robot_action == 4:
-                if debug: print('GAME INFO: standing still ☺')
-                reward = 0.5
+            elif game_info.puck_is_at_the_bottom:
+                if game_info.distance_decreased:
+                    if debug: print('GAME INFO: decreased distance ☺')
+                    reward = 0.5
+                else:
+                    if debug: print('GAME INFO: increased distance ☹')
+                    reward = -0.5
             else:
-                if debug: print('GAME INFO: not standing still ☹')
+                if game_info.in_the_target:
+                    if robot_action == 4 or robot_action == 9:
+                        if debug: print('GAME INFO: standing still in the target ☺')
+                        reward = 0.5
+                    else:
+                        if debug: print('GAME INFO: not standing still in the target ☹')
+                        reward = -0.5
+                else:
+                    if debug: print('GAME INFO: robot is not in the target ☹')
+                    reward = -0.5
+                    
+        elif self.episode == 'missing':
+            if robot_action == 4:
+                if debug: print('GAME INFO: standing while missing ☺')
+                reward =  0.5
+            else:
+                if debug: print('GAME INFO: moving while missing ☹')
+                reward = -0.5
+                
+        elif self.episode == 'random':
+            if robot_action == 9:
+                if debug: print('GAME INFO: standing while random ☺')
+                reward =  0.5
+            else:
+                if debug: print('GAME INFO: moving while random ☹')
                 reward = -0.5
     
         reward = np.clip(reward, -1.0, 1.0)
-        state = self.processor.process_observation(game_info.frame)
+        
+        if self.episode == 'random':
+            terminal = False
+            state = np.random.ranf((9, 128, 128))
+            state -= np.mean(state)
+            state /= np.std(state)
+        else:
+            state = self.processor.process_observation(game_info.frame)
 
         return state, reward, terminal, {'robot_action': robot_action, 'human_action': human_action}
 
-    def reset(self):
-        # Reset the environment
-        self.game.reset()
-        # Fill in the current_state
-        stand_action = self.processor.process_action(4)
-        for _ in range(3):
-            game_info = self.game.step(robot_action=stand_action, human_action=stand_action)
-            state = self.processor.process_observation(game_info.frame)
-        return state
 
 
 
